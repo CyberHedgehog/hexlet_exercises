99897275175e314e7fe1b3e129868ee586ebfcfe
Dmitriy Dobrenkiy <reg.dobr@live.ru>
2018-08-08 17:31:53 +0300

    homework: assync1

--- src/assync/lesson_1.js
+++ src/assync/lesson_1.js
@@ -7,9 +7,7 @@ const data2 = `Hello!
 This is second file!
 This, is string at new file
 `;
-
-//const newArr = data.split('\n').slice(0, -1);
 const diff = (str1, str2) => {
     const arr1 = str1.split('\n').slice(0, -1);
     const arr2 = str2.split('\n').slice(0, -1);
-}
+};
