0b8931e83975dd43db8e89c5abd2ef447a3e735c
Dmitriy Dobrenkiy <reg.dobr@live.ru>
2018-08-13 10:32:51 +0300

    test Waterfall

---
+++ src/assync/callback_hell/hellFunction.js
@@ -0,0 +1,20 @@
+import waterfall from './waterfall';
+import fs from 'fs';
+
+const sourceFile = 'test/file1';
+const destFile = 'test/file2';
+
+const functions = [
+    cb => fs.readFile(sourceFile, (err, body) => {
+        cb(err, `${body}/appended`);
+    }),
+    (body, cb) => fs.writeFile(destFile, body, err => {
+            cb(err);
+        }),
+];
+
+waterfall(functions, err => {
+    if (err) {
+        return console.log(err);
+    };
+});
